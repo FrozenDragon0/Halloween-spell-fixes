e6b4da58e71b5dee6c05262fa56df4777719df97
FrozenDragon <raginglightning833@gmail.com>
2025-10-24 00:05:44 +0800

    Cow Mangler 5000 Pumpkin Bombs Spell fixes.
    Adds fixes for the Cow Mangler 5000 if the pumpkin bombs spell is applied which currently does nothing to the weapon in the base game.

---
+++ c_tf_projectile_energy_ball.cpp
@@ -0,0 +1,130 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: 
+//
+//=============================================================================
+
+#include "cbase.h"
+#include "c_tf_projectile_energy_ball.h"
+#include "particles_new.h"
+#include "SpriteTrail.h"
+#include "c_tf_player.h"
+#include "collisionutils.h"
+#include "util_shared.h"
+#include "tf_weapon_rocketlauncher.h"
+#include "tf_gamerules.h"
+
+//-----------------------------------------------------------------------------
+IMPLEMENT_NETWORKCLASS_ALIASED( TFProjectile_EnergyBall, DT_TFProjectile_EnergyBall )
+
+BEGIN_NETWORK_TABLE( C_TFProjectile_EnergyBall, DT_TFProjectile_EnergyBall )
+	RecvPropBool( RECVINFO( m_bChargedShot ) ),
+	RecvPropVector( RECVINFO( m_vColor1 ) ),
+	RecvPropVector( RECVINFO( m_vColor2 ) )
+END_NETWORK_TABLE()
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+C_TFProjectile_EnergyBall::C_TFProjectile_EnergyBall( void )
+{
+	pEffect = NULL;
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+C_TFProjectile_EnergyBall::~C_TFProjectile_EnergyBall( void )
+{
+	if ( pEffect )
+	{
+		ParticleProp()->StopEmission( pEffect );
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+void C_TFProjectile_EnergyBall::CreateTrails( void )
+{
+	if ( IsDormant() )
+		return;
+
+	if ( pEffect )
+	{
+		ParticleProp()->StopEmission( pEffect );
+		pEffect = NULL;
+	}
+
+	bool bDeflected = m_iCachedDeflect != GetDeflected();
+	bool bUsingHalloween = false;
+
+	// Check for Halloween spell
+	int iHalloweenSpell = 0;
+	if ( TF_IsHolidayActive( kHoliday_HalloweenOrFullMoon ) )
+	{
+		CALL_ATTRIB_HOOK_INT_ON_OTHER( GetOriginalLauncher(), iHalloweenSpell, halloween_pumpkin_explosions );
+	}
+
+	if ( pEffect == NULL )
+	{
+		ParticleProp()->Init( this );
+
+		// Handle Halloween spell particles
+		if ( iHalloweenSpell > 0 )
+		{
+			const char *pszHalloweenParticle = NULL;
+			
+			// Use charged or normal Halloween particles based on shot type
+			if ( m_bChargedShot )
+			{
+				pszHalloweenParticle = ( GetTeamNumber() == TF_TEAM_RED ) ? "drg_cow_rockettrail_charged_halloween" : "drg_cow_rockettrail_charged_halloween_blue";
+			}
+			else
+			{
+				pszHalloweenParticle = ( GetTeamNumber() == TF_TEAM_RED ) ? "drg_cow_rockettrail_halloween" : "drg_cow_rockettrail_halloween_blue";
+			}
+			
+			pEffect = ParticleProp()->Create( pszHalloweenParticle, PATTACH_ABSORIGIN_FOLLOW, 0 );
+			bUsingHalloween = true;
+		}
+		else
+		{
+			// Use standard Cow Mangler trails
+			pEffect = ParticleProp()->Create( GetTrailParticleName(), PATTACH_ABSORIGIN_FOLLOW, 0 );
+		}
+
+		if ( pEffect && !bUsingHalloween )
+		{
+			if ( bDeflected )
+			{
+				if ( GetTeamNumber() == TF_TEAM_BLUE )
+				{
+					pEffect->SetControlPoint( CUSTOM_COLOR_CP1, TF_PARTICLE_WEAPON_BLUE_1 );
+					pEffect->SetControlPoint( CUSTOM_COLOR_CP2, TF_PARTICLE_WEAPON_BLUE_2 );
+				}
+				else
+				{
+					pEffect->SetControlPoint( CUSTOM_COLOR_CP1, TF_PARTICLE_WEAPON_RED_1 );
+					pEffect->SetControlPoint( CUSTOM_COLOR_CP2, TF_PARTICLE_WEAPON_RED_2 );
+				}
+			}
+			else
+			{
+				pEffect->SetControlPoint( CUSTOM_COLOR_CP1, m_vColor1 );
+				pEffect->SetControlPoint( CUSTOM_COLOR_CP2, m_vColor2 );
+			}
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Purpose: 
+//-----------------------------------------------------------------------------
+const char *C_TFProjectile_EnergyBall::GetTrailParticleName( void )
+{
+	if ( m_bChargedShot )
+		return ( GetTeamNumber() == TF_TEAM_RED ) ? "drg_cow_rockettrail_charged" : "drg_cow_rockettrail_charged_blue";
+	else
+		return ( GetTeamNumber() == TF_TEAM_RED ) ? "drg_cow_rockettrail_normal" : "drg_cow_rockettrail_normal_blue";
+}
